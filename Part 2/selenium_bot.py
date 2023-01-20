d84a4a8bd406b78a7839235f84153295fb69bdcd
Carlos Garcia <carlosgarcia@Carloss-MacBook-Air.local>
2023-01-19 20:12:49 -0800

    removed some hidden files

--- Part 2/selenium_bot.py
+++ Part 2/selenium_bot.py
@@ -5,4 +5,15 @@ Created on Wed Jan 18 19:00:06 2023
 
 @author: carlosgarcia
 """
-import os
+#%% Tools
+from selenium.webdriver.chromium.service import ChromiumService
+from selenium import webdriver
+import time
+
+#%% Setting Up Driver
+chrome_service = ChromiumService(executable_path="Part 2/chromedriver")
+driver = webdriver.Chrome(service=chrome_service)
+kaggle_competition_homepage = "https://www.kaggle.com/competitions?listOption=completed&sortOption=recentlyClosed&prestigeFilter=money"
+driver.get(kaggle_competition_homepage)
+time.sleep(2)
+driver.quit()
